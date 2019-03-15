4f6a0775a0004be2f0447da985774b766560ce39
MJQuesada <MJQuesada45@gmail.com>
2019-03-15 17:05:31 -0600

    Up to user story 3

--- src/cashRegister.java
+++ src/cashRegister.java
@@ -1,9 +1,34 @@
+import java.util.Scanner;
 
 public class cashRegister {
 
 	public static void main(String[] args) {
+
+		int price;
+		int cashTendered;
+		int change;
 		
+		pricePrompt ();
 
 	}
-
+	public static void pricePrompt() {
+		Scanner keyboard = new Scanner(System.in);
+		
+		int methodPrice;
+		
+		System.out.println("What is the price of this item?");
+		methodPrice = keyboard.nextInt();
+		
+	}
+	public static int amountTendered () {
+		Scanner keyboard = new Scanner (System.in);
+		
+		int methodCashTendered;
+		
+		System.out.println("How much money did you tender?");
+		methodCashTendered = keyboard.nextInt();
+	}
+	public static void correctAmountTendered () {
+		
+	}
 }
